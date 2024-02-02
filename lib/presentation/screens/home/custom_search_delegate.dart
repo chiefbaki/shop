f00a36fd65dccfe8d34abf35f65fa9f08bee9f88
Arthur <rustamovartur675@gmail.com>
2024-02-03 01:09:55 +0600

    Полностью доработан механихм поиска в Searchbar

--- lib/presentation/screens/home/custom_search_delegate.dart
+++ lib/presentation/screens/home/custom_search_delegate.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:shop/data/model/my_products.dart';
 import 'package:shop/data/model/product_model.dart';
 import 'package:shop/presentation/widgets/list_of_products.dart';
+import 'package:shop/presentation/widgets/product_card.dart';
 
 class CustomSearchDelegate extends SearchDelegate {
   List<Product> listOfProducts = MyProducts.allProducts;
@@ -33,11 +34,39 @@ class CustomSearchDelegate extends SearchDelegate {
 
   @override
   Widget buildResults(BuildContext context) {
-    return ListOfProducts();
+    List<Product> matchQuery = [];
+    for (var product in listOfProducts) {
+      if (product.name.toLowerCase().contains(query.toLowerCase())) {
+        matchQuery.add(product);
+      }
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(13.0),
+      child: GridView.builder(
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: (100 / 127),
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        itemCount: matchQuery.length,
+        itemBuilder: (BuildContext context, int index) {
+          final allProducts = matchQuery[index];
+          return ProductCard(
+            product: allProducts,
+          );
+        },
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return ListOfProducts();
+    return Padding(
+      padding: const EdgeInsets.all(13.0),
+      child: ListOfProducts(),
+    );
   }
 }
